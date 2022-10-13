6d3aaff65d6569edf8461c7e35f010624b971f2e
MathanRajS <MathanRajS@Mathanraj>
2022-10-13 14:50:51 +0530

    Spring datajpa bookapp

---
+++ spring-datajpa-bookapp/src/main/java/com/bookapp/model/Book.java
@@ -0,0 +1,64 @@
+package com.bookapp.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Book {
+@Id
+private Integer bookId;
+private String title;
+private String category;
+private String author;
+@Column(name="cost")
+private double price;
+public Book() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+public Book(Integer bookId, String title, String category, String author, double price) {
+	super();
+	this.bookId = bookId;
+	this.title = title;
+	this.category = category;
+	this.author = author;
+	this.price = price;
+}
+public Integer getBookId() {
+	return bookId;
+}
+public void setBookId(Integer bookId) {
+	this.bookId = bookId;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getCategory() {
+	return category;
+}
+public void setCategory(String category) {
+	this.category = category;
+}
+public String getAuthor() {
+	return author;
+}
+public void setAuthor(String author) {
+	this.author = author;
+}
+public double getPrice() {
+	return price;
+}
+public void setPrice(double price) {
+	this.price = price;
+}
+@Override
+public String toString() {
+	return "Book [bookId=" + bookId + ", title=" + title + ", category=" + category + ", author=" + author + ", price="
+			+ price + "]";
+}
+
+}
